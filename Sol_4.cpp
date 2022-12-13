0f729b9216d1eee2b4a9e5c219b3963dfba7f6d6
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-13 18:33:55 +0530

    Create Sol_4.cpp

---
+++ Sol_4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T,X,Y;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> X >> Y;
+		if (X >= Y ) {
+			cout << "Yes" << endl;
+		}
+		else {
+			cout << "NO"<< endl;
+		}
+	}
+	return 0;
+}
