faf5b923b019c3a3806ce086e344bb0b0a5bd1e9
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-13 16:10:48 +0530

    Create Sol_3.cpp

---
+++ Sol_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X, Y;
+		cin >> X >> Y;
+		if (X < Y) {
+			cout << "Yes" << endl;
+		}
+		else {
+			cout <<"No" << endl;
+		}
+	return 0;
+}
