16de0fce14b1dd918f85793fdc5bf356586e75f6
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-13 22:18:20 +0530

    Create Sol_6.cpp

---
+++ Sol_6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X,Y,P;
+	cin>>T;
+	for(int i=0; i<T; i++){
+	    cin>>X>>Y;
+	   P =(X*10) +(Y*90); 
+	   cout<<P<<endl;
+	}
+	return 0;
+}
