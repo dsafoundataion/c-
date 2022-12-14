f97f95d79fa46ed814adfa857c07aa2bc5681685
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-14 19:19:03 +0530

    Create Sol_11.cpp

---
+++ Sol_11.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y,z;
+	cin>>t;
+	for(int i =0; i<t; i++){
+	    cin>>x>>y>>z;
+	    int sum = ((4*x)+(2*y)+(0*z));
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
