b7cec970e7e37fd6a67d915b97260d9292ea29e6
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-15 18:08:07 +0530

    Create Sol_28.cpp

---
+++ Sol_28.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    cin>>x;
+	    if(x>=1  && x<=4){
+	        cout<<"Yes"<<endl;
+	    }else{
+	        cout<<"No"<<endl;
+	        
+	    }
+	}
+	return 0;
+}
