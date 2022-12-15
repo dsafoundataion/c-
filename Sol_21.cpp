58fbce318436b43ddb71ca1df24de9a206564818
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-15 14:08:53 +0530

    Create Sol_21.cpp

---
+++ Sol_21.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    cin>>a>>b;
+	    int gap = a+b;
+	    cout<<gap<<endl;
+	}
+	return 0;
+}
