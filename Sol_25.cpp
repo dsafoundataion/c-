cfcca390075f699032ebf2bbe57571b40835cfb8
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-15 15:44:43 +0530

    Create Sol_25.cpp

---
+++ Sol_25.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    cin>>x>>y;
+	    if(x>=y){
+	        cout<<0<<endl;
+	    }else{
+	        cout<<(y-x)<<endl;
+	    }
+	}
+	return 0;
+}
