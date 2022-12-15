cb60fcac63c51cc6965d191435857f0d2d825332
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-15 18:28:23 +0530

    Create Sol_29.cpp

---
+++ Sol_29.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int rs,a,b;
+	cin>>rs>>a>>b;
+	if((rs)>=((a*1)+(b*1))){
+	    cout<<"Yes"<<endl;
+	}else{
+	    cout<<"No"<<endl;
+	    
+	}
+	return 0;
+}
