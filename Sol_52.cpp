abb1bf817324680452d25490510b8a2b1b3d3a47
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-18 01:44:20 +0530

    Create Sol_52.cpp

---
+++ Sol_52.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,c;
+	cin>>t;
+	for(int i=0; i<t;i++){
+	cin>>c;
+	if(c>20){
+	    cout<<"Hot"<<endl;
+	}else{
+	    cout<<"Cold"<<endl;
+	}
+	}
+	return 0;
+}
