1d159374f96047ef5aba8c1ac72b3275a4f30d24
dsa_developers <120314431+dsafoundataion@users.noreply.github.com>
2022-12-17 23:47:24 +0530

    Create Sol_43.cpp

---
+++ Sol_43.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n,k;
+	cin>>t;
+	for(int i=0; i<t; i++){
+	    cin>>n>>k;
+	    cout<<n-k<<endl;
+	}
+	return 0;
+}
